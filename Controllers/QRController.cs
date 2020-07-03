de59b633e76ad5646eac8a89425533f6f5166eaf
acp-anjan <paudelanjanchandra@gmail.com>
2020-07-03 19:48:37 +0545

    @anjan initial commit

---
+++ Controllers/QRController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+
+
+namespace NQRcodeN.Controllers
+{
+    public class QRController : Controller
+    {
+        public ActionResult Index()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            //JsonOptions a = new {productId:1}
+            //string json = new JavaScriptSerializer().Serialize(new
+            //{
+            //    message = new { text = "test sms" },
+            //    endpoints = new[] { "dsdsd", "abc", "123" }
+            //});
+            
+            var qrObject = new { ProductId = 1, CategoryId = 2, BrandId = 3, ProductName = "Hello" };
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrObject.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            var bitmapBytes = BitmapToBytes(qrCodeImage); //Convert bitmap into a byte array
+            /*return File(bitmapBytes, "image/jpeg");*/ //Return as file result
+            var a =  File(bitmapBytes, "image/png");
+            return a;
+        }
+        // This method is for converting bitmap into a byte array
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+
+            
+        }
+    }
+}
